9702f4ded4b03f34ac826b1003cefd25d736b6ce
TibebeJs <Tibebes.js@gmail.com>
2021-01-12 15:30:01 +0300

    show success message upon start

--- selfbot/selfbot.py
+++ selfbot/selfbot.py
@@ -8,13 +8,14 @@ class SelfBot:
         self.initilize()
 
     def initilize(self):
+        self.client.start()
         self.activateCommand(commands.ls(self))
         self.activateCommand(commands.helper(self))
         # self.activateCommand(commands.rm(self)) # not ready for use yet.
+        self.client.loop.run_until_complete(self.client.send_message("me", "`[✓] Bot is up and running...`\n\nsend `.help` to see available commands"))
 
     def activateCommand(self, command):
         self.active_commands.append(command)
 
     def run(self):
-        with self.client:
-            self.client.run_until_disconnected()
+        self.client.run_until_disconnected()
